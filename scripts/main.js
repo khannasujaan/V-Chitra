c7e5293cfea060ccc6e019248250c243fb63ddc9
khannasujaan <khannasujaan@gmail.com>
2026-03-23 03:35:15 +0530

    refactor: add eventlistener in toolbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("drawCanvas");
 const ctx = canvas.getContext('2d');
+const toolBar = document.querySelector(".toolbar")
 let drawingRn = false;
 var color = "#000000";
 var mode = "pointer";
@@ -11,28 +12,40 @@ function canvasSize(){
 
 canvasSize();
 window.addEventListener('resize', canvasSize);
+toolBar.addEventListener('click', (event) => {
+    if (event.target.className == "tool"){
+        if (event.target.id == "undo"){
+            ctx.restore();
+        } else {
+            mode = event.target.id;
+            console.log(mode);
+        }
+    }
+});
+
 canvas.addEventListener('mousedown', (event) => {
     drawingRn = true;
     if (mode=="pointer"){
     
-    } else {
+    } else if (mode == "brush"){
         ctx.fillStyle = color;
         ctx.fillRect(event.clientX, event.clientY, 7, 7);
         console.log("Drawing being made");
     }
-})
+});
 canvas.addEventListener('mouseup', (event) => {
     drawingRn = false;
     console.log("Drawing stopped");
-})
+    ctx.save();
+});
 canvas.addEventListener('mousemove', (event) => {
     if (drawingRn){
         if (mode=="pointer"){
-
-        } else {
+            
+        } else if (mode == "brush"){
             ctx.fillStyle = color;
             ctx.fillRect(event.clientX, event.clientY, 7, 7);
             console.log("Drawing being made");
         }
     }
-})
+});
