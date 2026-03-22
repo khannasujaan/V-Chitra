af5325f8863415d14c26ca499cdbb53beb059c9b
khannasujaan <khannasujaan@gmail.com>
2026-03-23 04:21:43 +0530

    fix: fix the dotted line in brush mode

--- scripts/main.js
+++ scripts/main.js
@@ -16,14 +16,14 @@ canvasSize();
 window.addEventListener('resize', canvasSize);
 toolBar.addEventListener('click', (event) => {
     if (event.target.className == "tool"){
-        let toolbarelements = document.getElementsByClassName("tool");
-        for (let i = toolbarelements.length - 1; i >= 0; i--){
-            toolbarelements[i].style.background = "#FFFFFF";
-        }
         if (event.target.id == "undo"){
             ctx.restore();
             ctx.restore();
         } else {
+            let toolbarelements = document.getElementsByClassName("tool");
+            for (let i = toolbarelements.length - 1; i >= 0; i--){
+                toolbarelements[i].style.background = "#FFFFFF";
+            }
             mode = event.target.id;
             event.target.style.background = "#a1a1a1";
             console.log(mode);
@@ -38,6 +38,8 @@ canvas.addEventListener('mousedown', (event) => {
     } else if (mode == "brush"){
         ctx.fillStyle = color;
         ctx.fillRect(event.clientX, event.clientY, 2, 2);
+        tempCoords = [event.clientX, event.clientY];
+        ctx.moveTo(tempCoords[0], tempCoords[1]);
         console.log("Drawing being made");
     } else if (mode == "line"){
             ctx.fillStyle = color;
@@ -56,10 +58,12 @@ canvas.addEventListener('mouseup', (event) => {
     drawingRn = false;
     console.log("Drawing stopped");
     ctx.save();
-    if (mode == "line"){
+    if (mode == 'brush'){
+
+    } else if (mode == "line"){
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
-    } if (mode == "rect"){
+    } else if (mode == "rect"){
         ctx.lineTo(tempCoords[0], event.clientY);
         ctx.lineTo(event.clientX, event.clientY);
         ctx.moveTo(tempCoords[0], tempCoords[1]);
@@ -74,7 +78,9 @@ canvas.addEventListener('mousemove', (event) => {
             
         } else if (mode == "brush"){
             ctx.fillStyle = color;
-            ctx.fillRect(event.clientX, event.clientY, 2, 2);
+            ctx.lineTo(event.clientX, event.clientY, 2, 2);
+            ctx.stroke();
+            tempCoords = [event.clientX, event.clientY];
             console.log("Drawing being made");
         } 
     }
